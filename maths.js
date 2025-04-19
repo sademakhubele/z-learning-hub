f8fb601c9aa3ff239a897c082106052c1d841c10
sade makhubele <smakhubele1994@gmail.com>
2025-04-19 18:29:56 +0200

    created functionality to refresh button

--- maths.js
+++ maths.js
@@ -2,6 +2,9 @@ function checkAnswer(button, correctAnswer) {
     const resultBox = button.parentElement.querySelector('.quiz-result');
     const userAnswer = button.innerText.trim();
 
+    console.log("User Answer:", userAnswer, "| Correct Answer:", correctAnswer);
+
+
     if (userAnswer === correctAnswer) {
         resultBox.textContent = "✅ Correct!";
         resultBox.style.color = "green";
@@ -9,4 +12,13 @@ function checkAnswer(button, correctAnswer) {
         resultBox.textContent = "❌ Try again!";
         resultBox.style.color = "red";
     }
+}
+
+
+function resetQuiz(button) {
+    const quizBox = button.parentElement;
+    const resultBox = quizBox.querySelector('.quiz-result');
+
+    resultBox.textContent = "";
+    button.style.display = "";
 }
